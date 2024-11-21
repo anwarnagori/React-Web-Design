8a05d79a991ff3280d7b090e362615258da1da3f
anwarnagori <ar1498137@gmail.com>
2024-11-21 23:13:39 +0500

    starting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "../assets/css/style.css";
+import "../assets/css/responsive.css";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Details from "./Details";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
+};
+
+export default App;
